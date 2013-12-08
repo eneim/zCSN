46d7033ec2d43136cc985ac6d2cbff97385e6975
eneim <mr.nguyenhoainam@gmail.com>
2013-12-09 01:21:15 +0900

    Add ViewPager

--- src/org/eneim/tempapp/fragments/MainFragmentActivity.java
+++ src/org/eneim/tempapp/fragments/MainFragmentActivity.java
@@ -1,5 +1,7 @@
 package org.eneim.tempapp.fragments;
 
+import java.util.ArrayList;
+
 import org.eneim.tempapp.R;
 
 import android.content.Context;
@@ -8,79 +10,114 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.widget.Toast;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 
 import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainFragmentActivity extends BaseActivity {
+public class MainFragmentActivity extends SlidingFragmentActivity {
 
-	private ViewPager mViewPager;
 	private Context mContext;
-	private SlidingMenu mSlidingMenu;
-	
-	public MainFragmentActivity() {
-		super(R.string.app_name);
-		// TODO Auto-generated constructor stub
-	}
+	private ViewPager mViewPager;
+	private MainListFragmentAdapter mListAdapter;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pager);
+		setBehindContentView(R.layout.menu_frame);
+
 		mContext = this;
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mListAdapter = new MainListFragmentAdapter(getSupportFragmentManager());
+		mViewPager.setAdapter(mListAdapter);
+		//mViewPager.setCurrentItem(0);
 		
-		mViewPager = new ViewPager(this);
-		mViewPager.setId("mainUI".hashCode());
-		mViewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager()));
-		setContentView(mViewPager);	
+		// customize the SlidingMenu
+		SlidingMenu sm = getSlidingMenu();
+		sm.setShadowWidthRes(R.dimen.shadow_width);
+		sm.setShadowDrawable(R.drawable.shadow);
+		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		sm.setFadeDegree(0.35f);
+		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+		mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
+			@Override
+			public void onPageScrollStateChanged(int arg0) { }
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) { }
+
+			@Override
+			public void onPageSelected(int position) {
+				switch (position) {
+				case 0:
+					//getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+					getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+					
+					break;
+				default:
+					getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+					break;
+				}
+			}
+
+		});
 		
-		mSlidingMenu = getSlidingMenu();
+		mViewPager.setCurrentItem(0);
+		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 	
 	@Override
-	public boolean onCreateOptionsMenu(Menu mMenu) {
-		MenuInflater mMenuInflater = getSupportMenuInflater();
-		mMenuInflater.inflate(R.menu.main, mMenu);
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		getSupportMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
 		switch (item.getItemId()) {
-		case R.id.action_settings:
-			Toast.makeText(mContext, "refresh press", Toast.LENGTH_SHORT).show();
-			Log.d("Action Menu test", item.getItemId() + "");
-			break;
 		case android.R.id.home:
-			toggle();		
-		default:
-			break;
-		}	
+			toggle();
+			return true;
+		}
 		
-		return true;
-	}	
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public class MainListFragmentAdapter extends FragmentPagerAdapter {
 
-	public class MainPagerAdapter extends FragmentPagerAdapter {
-
-		public MainPagerAdapter(FragmentManager fm) {
+		private ArrayList<Fragment> mFragments;
+		
+		private FragmentManager mFragmentManager;
+		
+		public MainListFragmentAdapter(FragmentManager fm) {
 			super(fm);
 			// TODO Auto-generated constructor stub
+			mFragments = new ArrayList<Fragment>();
+			mFragments.add(new DataListFragment());
+			mFragments.add(new DataListFragment());
+			mFragments.add(new DataListFragment());
+			mFragments.add(new DataListFragment());
+			
 		}
 
 		@Override
 		public Fragment getItem(int arg0) {
-			// TODO Auto-generated method stub
-			return null;
+			// TODO Auto-generated method stub			
+			return mFragments.get(arg0);
 		}
 
 		@Override
 		public int getCount() {
 			// TODO Auto-generated method stub
-			return 0;
+			return mFragments.size();
 		}
 		
 	}
