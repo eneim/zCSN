0935149823e3696fe2467cd10909a1ffc219aaa3
eneim <mr.nguyenhoainam@gmail.com>
2013-12-09 17:16:10 +0900

    add listview

--- src/org/eneim/tempapp/fragments/MainFragmentActivity.java
+++ src/org/eneim/tempapp/fragments/MainFragmentActivity.java
@@ -22,27 +22,22 @@ public class MainFragmentActivity extends SlidingFragmentActivity {
 	private Context mContext;
 	private ViewPager mViewPager;
 	private MainListFragmentAdapter mListAdapter;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+
+		// layout out viewpager on the top
 		setContentView(R.layout.activity_pager);
+
+		// menu is set to behind
 		setBehindContentView(R.layout.menu_frame);
 
 		mContext = this;
 		mViewPager = (ViewPager) findViewById(R.id.pager);
 		mListAdapter = new MainListFragmentAdapter(getSupportFragmentManager());
 		mViewPager.setAdapter(mListAdapter);
-		//mViewPager.setCurrentItem(0);
-		
-		// customize the SlidingMenu
-		SlidingMenu sm = getSlidingMenu();
-		sm.setShadowWidthRes(R.dimen.shadow_width);
-		sm.setShadowDrawable(R.drawable.shadow);
-		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-		sm.setFadeDegree(0.35f);
-		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
 
 		mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
 			@Override
@@ -57,7 +52,7 @@ public class MainFragmentActivity extends SlidingFragmentActivity {
 				case 0:
 					//getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
 					getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
-					
+
 					break;
 				default:
 					getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
@@ -66,19 +61,28 @@ public class MainFragmentActivity extends SlidingFragmentActivity {
 			}
 
 		});
-		
+
 		mViewPager.setCurrentItem(0);
-		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+
+		// customize the SlidingMenu
+		SlidingMenu mSlidingMenu = getSlidingMenu();
+		mSlidingMenu.setShadowWidthRes(R.dimen.shadow_width);
+		mSlidingMenu.setShadowDrawable(R.drawable.shadow);
+		mSlidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		mSlidingMenu.setFadeDegree(0.35f);
+		mSlidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		//mSlidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+		
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// TODO Auto-generated method stub
 		getSupportMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
@@ -87,25 +91,23 @@ public class MainFragmentActivity extends SlidingFragmentActivity {
 			toggle();
 			return true;
 		}
-		
+
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	public class MainListFragmentAdapter extends FragmentPagerAdapter {
 
 		private ArrayList<Fragment> mFragments;
 		
-		private FragmentManager mFragmentManager;
-		
 		public MainListFragmentAdapter(FragmentManager fm) {
 			super(fm);
-			// TODO Auto-generated constructor stub
+			// add fragments to pager here
 			mFragments = new ArrayList<Fragment>();
-			mFragments.add(new DataListFragment());
-			mFragments.add(new DataListFragment());
-			mFragments.add(new DataListFragment());
-			mFragments.add(new DataListFragment());
-			
+			mFragments.add(new DataListFragment(getApplicationContext(), "http://chiasenhac.com/mp3/vietnam/"));
+			mFragments.add(new DataListFragment(getApplicationContext(), "http://chiasenhac.com/mp3/us-uk/"));
+			mFragments.add(new DataListFragment(getApplicationContext(), "http://chiasenhac.com/mp3/chinese/"));
+			mFragments.add(new DataListFragment(getApplicationContext(), "http://chiasenhac.com/mp3/korea/"));
+			mFragments.add(new DataListFragment(getApplicationContext(), "http://chiasenhac.com/mp3/other/"));
 		}
 
 		@Override
@@ -119,6 +121,6 @@ public class MainFragmentActivity extends SlidingFragmentActivity {
 			// TODO Auto-generated method stub
 			return mFragments.size();
 		}
-		
+
 	}
 }
